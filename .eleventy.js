10d24e84c66fefc8957b146f88e1c8c68a8e36a8
Jake Mulley <jakemulley@users.noreply.github.com>
2021-06-06 11:42:00 +0100

    Add some basic styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
+const now = String(Date.now())
+
 module.exports = function (eleventyConfig) {
+  // Add watch targets
+  eleventyConfig.addWatchTarget('./_tmp/style.css')
+
   // Passthrough static files
   eleventyConfig.addPassthroughCopy('./website/CNAME')
+  eleventyConfig.addPassthroughCopy({
+    './_tmp/style.css': './style.css'
+  })
+
+  // Add shortcode to get the current time
+  eleventyConfig.addShortcode('now', function () {
+    return now
+  })
 
   return {
     dir: {
