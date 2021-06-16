8e110205605984e64301e5713e254e54bb68217c
Jake Mulley <jakemulley@users.noreply.github.com>
2021-06-16 18:59:33 +0100

    Minify and purge CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    require('postcss-import')
+    require('postcss-import'),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./build/**/*.html']
+    }),
+    require('cssnano')({
+      preset: 'default'
+    })
   ]
 }
